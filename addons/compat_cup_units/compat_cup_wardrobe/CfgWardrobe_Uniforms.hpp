dad8061c1217c37b0a746e7f7b9667bbbfefcb93
Andx <andx667@protonmail.com>
2025-05-10 00:58:16 +0200

    added more uniforms

--- addons/compat_cup_units/compat_cup_wardrobe/CfgWardrobe_Uniforms.hpp
+++ addons/compat_cup_units/compat_cup_wardrobe/CfgWardrobe_Uniforms.hpp
@@ -17,6 +17,24 @@ class tshirtKnee: EGVAR(wardrobe,base) {\
     };\
 }
 
+#define CUP_UNIFORM_3(uniformBase,uniformVariant1,uniformVariant2)\
+class uniformBase: EGVAR(wardrobe,base) {\
+    class modifiableTo {\
+        class uniformVariant1;\
+        class uniformVariant2;\
+    };\
+};\
+class uniformVariant1: EGVAR(wardrobe,base) {\
+    class modifiableTo {\
+        class uniformBase;\
+    };\
+};\
+class uniformVariant2: EGVAR(wardrobe,base) {\
+    class modifiableTo {\
+        class uniformBase;\
+    };\
+}
+
 //Army of the Czech Republic
 UNIFORM_SLEEVES(CUP_U_B_CZ_DST_NoKneepads,CUP_U_B_CZ_DST_Kneepads_Sleeve);
 UNIFORM_SLEEVES(CUP_U_B_CZ_DST_Kneepads,CUP_U_B_CZ_DST_Kneepads_Gloves);
@@ -38,6 +56,33 @@ UNIFORM_SLEEVES(CUP_U_O_RUS_VSR93_VDV,CUP_U_O_RUS_VSR93_VDV_rolled_up);
 UNIFORM_SLEEVES(CUP_U_O_RUS_VSR93_VDV_rolled_up,CUP_U_O_RUS_VSR93_VDV_rolled_up_gloves2);
 UNIFORM_SLEEVES(CUP_U_O_RUS_VSR98_VDV,CUP_U_O_RUS_VSR98_VDV_rolled_up);
 UNIFORM_SLEEVES(CUP_U_O_RUS_VSR98_VDV_rolled_up,CUP_U_O_RUS_VSR98_VDV_rolled_up_gloves2);
+UNIFORM_SLEEVES(CUP_U_O_RUS_M88_MSV,CUP_U_O_RUS_M88_MSV_rolled_up);
+UNIFORM_SLEEVES(CUP_U_O_RUS_M88_VDV,CUP_U_O_RUS_M88_VDV_rolled_up);
+
+
+// ToDo MACRO
+class CUP_U_O_RUS_Soldier_Masking_Suit_6Sh119_Winter_1: EGVAR(wardrobe,base) {
+    class modifiableTo {
+        class CUP_U_O_RUS_Soldier_Masking_Suit_6Sh119_Winter_2;
+        class CUP_U_O_RUS_Soldier_Masking_Suit_6Sh119_Winter_3;
+        class CUP_U_O_RUS_Soldier_Masking_Suit_6Sh119_Winter_4;
+    };
+};
+class CUP_U_O_RUS_Soldier_Masking_Suit_6Sh119_Winter_2: EGVAR(wardrobe,base) {
+    class modifiableTo {
+        class CUP_U_O_RUS_Soldier_Masking_Suit_6Sh119_Winter_1;
+    };
+};
+class CUP_U_O_RUS_Soldier_Masking_Suit_6Sh119_Winter_3: EGVAR(wardrobe,base) {
+    class modifiableTo {
+        class CUP_U_O_RUS_Soldier_Masking_Suit_6Sh119_Winter_1;
+    };
+};
+class CUP_U_O_RUS_Soldier_Masking_Suit_6Sh119_Winter_4: EGVAR(wardrobe,base) {
+    class modifiableTo {
+        class CUP_U_O_RUS_Soldier_Masking_Suit_6Sh119_Winter_1;
+    };
+};
 
 //CUP_U_O_RUS_VSR93_MSV ->CUP_U_O_RUS_VSR93_MSV_rolled_up
 //CUP_U_O_RUS_VSR93_MSV -> CUP_U_O_RUS_VSR93_worn_gloves_MSV
@@ -53,7 +98,7 @@ UNIFORM_SLEEVES(CUP_U_O_RUS_VSR98_VDV_rolled_up,CUP_U_O_RUS_VSR98_VDV_rolled_up_
 //CUP_U_O_RUS_VSR98_worn_VDV_rolled_up -> CUP_U_O_RUS_VSR98_worn_VDV_rolled_up_gloves2
 //CUP_U_O_RUS_VSR98_worn_VDV_rolled_up_gloves2 -> CUP_U_O_RUS_VSR98_worn_gloves_VDV
 
-// This could maybe be a MACRO
+// ToDo MACRO
 class CUP_U_O_RUS_Soldier_VKPO_Desert_1: EGVAR(wardrobe,base) {
     class modifiableTo {
         class CUP_U_O_RUS_Soldier_VKPO_Desert_2;
@@ -237,7 +282,59 @@ class CUP_U_O_RUS_Soldier_VKPO_Rolled_Winter_4: EGVAR(wardrobe,base) {
     components[] = {};
 };
 
+// Gorka
+CUP_UNIFORM_3(CUP_U_O_RUS_Gorka_Green,CUP_U_O_RUS_Gorka_Green_gloves2,CUP_U_O_RUS_Gorka_Green_gloves_kneepads);
+CUP_UNIFORM_3(CUP_U_O_RUS_Gorka_Partizan_A,CUP_U_O_RUS_Gorka_Partizan_A_gloves2,CUP_U_O_RUS_Gorka_Partizan_A_gloves_kneepads);
+CUP_UNIFORM_3(CUP_U_O_RUS_Gorka_Partizan,CUP_U_O_RUS_Gorka_Partizan_gloves2,CUP_U_O_RUS_Gorka_Partizan_gloves_kneepads);
+
+
 // British Armed Forces
 CUP_UNIFORM_TSHIRT(CUP_U_B_BAF_MTP_UBACSTSHIRT,CUP_U_B_BAF_MTP_UBACSTSHIRT_Gloves,CUP_U_B_BAF_MTP_UBACSTSHIRTKNEE);
 CUP_UNIFORM_TSHIRT(CUP_U_B_BAF_DPM_UBACSTSHIRT,CUP_U_B_BAF_DPM_UBACSTSHIRT_Gloves,CUP_U_B_BAF_DPM_UBACSTSHIRTKNEE);
 CUP_UNIFORM_TSHIRT(CUP_U_B_BAF_DDPM_UBACSTSHIRT,CUP_U_B_BAF_DDPM_UBACSTSHIRT_Gloves,CUP_U_B_BAF_DDPM_UBACSTSHIRTKNEE);
+
+// Crye
+UNIFORM_SLEEVES(CUP_U_CRYE_ATACSAU_Full,CUP_U_CRYE_ATACSAU_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_ATACSFG_Full,CUP_U_CRYE_ATACSFG_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_BLK_Full,CUP_U_CRYE_BLK_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_BLK_RUS_Full,CUP_U_CRYE_BLK_RUS_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_MCAM_NP_Full,CUP_U_CRYE_MCAM_NP_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_MCAM_RUS_Full,CUP_U_CRYE_MCAM_RUS_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_MCAM_NP2_Full,CUP_U_CRYE_MCAM_NP2_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_MCAM_RUS2_Full,CUP_U_CRYE_MCAM_RUS2_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_V1_Full,CUP_U_CRYE_V1_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_V2_Full,CUP_U_CRYE_V2_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_V3_Full,CUP_U_CRYE_V3_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_RGR_Full,CUP_U_CRYE_RGR_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_V2_Full,CUP_U_CRYE_V2_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_RGR_US_Full,CUP_U_CRYE_RGR_US_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_TAN_Full,CUP_U_CRYE_TAN_Roll);
+UNIFORM_SLEEVES(CUP_U_CRYE_TAN_US_Full,CUP_U_CRYE_TAN_US_Roll);  
+UNIFORM_SLEEVES(CUP_U_B_GER_Fleck_Crye,CUP_U_B_GER_Fleck_Crye2);
+UNIFORM_SLEEVES(CUP_U_B_GER_Crye,CUP_U_B_GER_Crye2);
+
+// German Army
+
+// Horizon Island Legion
+
+// Rebels
+BASE_PAIR(CUP_U_I_GUE_Anorak_01,CUP_U_I_GUE_Anorak_03);
+BASE_PAIR(CUP_U_I_GUE_Flecktarn2,CUP_U_I_GUE_Flecktarn3);
+BASE_PAIR(CUP_U_I_GUE_Flecktarn2,CUP_U_I_GUE_Flecktarn3);
+
+// PMC
+BASE_PAIR(CUP_I_B_PMC_Unit_30,CUP_I_B_PMC_Unit_29);
+BASE_PAIR(CUP_I_B_PMC_Unit_27,CUP_I_B_PMC_Unit_26);
+BASE_PAIR(CUP_I_B_PMC_Unit_25,CUP_I_B_PMC_Unit_18);
+BASE_PAIR(CUP_I_B_PMC_Unit_34,CUP_I_B_PMC_Unit_33);
+
+//Royal Army Corps of Sahrani
+BASE_PAIR(CUP_U_I_RACS_Desert_1,CUP_U_I_RACS_Desert_2);
+BASE_PAIR(CUP_U_I_RACS_mech_1,CUP_U_I_RACS_mech_2);
+BASE_PAIR(CUP_U_I_RACS_Urban_1,CUP_U_I_RACS_Urban_2);
+BASE_PAIR(CUP_U_I_RACS_WDL_1,CUP_U_I_RACS_WDL_2);
+
+// US Army
+BASE_PAIR(CUP_U_B_USArmy_Base,CUP_U_B_USArmy_TwoKnee);
+
+// USMC
